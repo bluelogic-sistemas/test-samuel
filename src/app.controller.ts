914d0183e2fb57af16978a9ca496e11d3cd77bfc
Samuel Belo <belo.samuel@gmail.com>
2024-08-05 09:43:05 -0300

    Correção de erro e conclusão de rotina

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,39 +4,40 @@ import { Person } from './person.interface';
 
 @Controller("persons")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
- 
-  @Get()
-  getPessoas(): Person[] {
-    return this.appService.findAll();
-  }
+    constructor(private readonly appService: AppService) { }
 
-  @Get(':id')
-  getPessoaById(@Param('id') id: number): Person {
-    return this.appService.findById(Number(id));
-  }
+    @Get()
+    getPessoas(): Person[] {
+        return this.appService.findAll();
+    }
 
-  @Post()
-  addPessoa(@Body('name') name: string, @Body('age') age: number): Person {
-    return this.appService.save(name, age);
-  }
+    @Get(':id')
+    getPessoaById(@Param('id') id: number): Person {
+        return this.appService.findById(Number(id));
+    }
 
-  @Put(':id')
-  updatePessoa(
-    @Param('id') id: number,
-    @Body('name') name: string,
-    @Body('age') age: number
-  ): Person {
-    return this.appService.update(Number(id), name, age);
-  }
+    @Post()
+    addPessoa(@Body('name') name: string, @Body('age') age: number): Person {
+        return this.appService.save(name, age);
+    }
 
-  @Delete(':id')
-  deletePessoa(@Param('id') id: number): void {
-    this.appService.delete(Number(id));
-  }
+    @Put(':id')
+    updatePessoa(
+        @Param('id') id: number,
+        @Body('name') name: string,
+        @Body('age') age: number
+    ): Person {
+        return this.appService.update(Number(id), name, age);
+    }
 
-  @Get("message")
-  getHello(): string {
-    return this.appService.init();
-  }
+    @Delete(':id')
+    deletePessoa(@Param('id') id: number): void {
+        this.appService.delete(Number(id));
+        return;
+    }
+
+    @Get("message")
+    getHello(): string {
+        return this.appService.init();
+    }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,33 +1,52 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Person } from './person.interface';
 
 @Injectable()
 export class AppService {
 
-  private persons: Person[] = [];
-  private idCounter = 1;
+    private persons: Person[] = [];
+    private idCounter = 1;
 
-  init(): string {
-    return Teste Bluelogic;
-  }
+    init(): string {
+        return "Hello World"
+    }
 
-  findAll(): Person[] {
-    return this.persons;
-  }
+    findAll(): Person[] {
+        return this.persons;
+    }
 
-  findById(id: number): Person {
-    return null;
-  }
+    findById(id: number): Person {
+        const person = this.persons.find((p) => p.id === id)
+        return person;
+    }
 
-  save(name: string, age: number): Person {
-    return null;
-  }
+    save(name: string, age: number): Person {
+        const newPerson: Person = {
+            id: this.idCounter++,
+            name: name,
+            age: age
+        }
 
-  update(id: number, name: string, age: number): Person {
-    return null;
-  }
+        this.persons.push(newPerson);
+        return newPerson;
+    }
 
-  delete(id: number): void {
-    
-  }
+    update(id: number, name: string, age: number): Person {
+        const person = this.persons.find((p) => p.id === id)
+        if (person) {
+            person.name = name;
+            person.age = age;
+            return person;
+        }
+
+        return null;
+    }
+
+    delete(id: number): void {
+        const index = this.persons.findIndex((p) => p.id === id)
+        if (index) {
+            this.persons.splice(index, 1);
+        }
+        return;
+    }
 }
